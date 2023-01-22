8a5425c96f48093b4cc798059ca1ee335884a521
Html0101 <102829836+Html0101@users.noreply.github.com>
2023-01-22 10:45:20 +0000

    Add files via upload

---
+++ ChatRoom.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBRAf50_pDv2HfyndLNSX56Z-EIrqZxNw",
+    authDomain: "letschatwebapp-f15f9.firebaseapp.com",
+    projectId: "letschatwebapp-f15f9",
+    storageBucket: "letschatwebapp-f15f9.appspot.com",
+    messagingSenderId: "271720585805",
+    appId: "1:271720585805:web:bcc90f65e4a09ba0cf5a04"
+  };
